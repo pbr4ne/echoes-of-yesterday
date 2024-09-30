d680e47e117a759948028c9886b02fe098780524
pbr4ne <pbr4ne@protonmail.com>
2024-09-29 22:11:55 -0700

    feat: right side sidebar

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -7,29 +7,52 @@
     <n-layout-header bordered style="padding-top: 5px">
       header
     </n-layout-header>
+
     <n-layout has-sider>
       <n-layout-sider
         bordered
         show-trigger
         collapse-mode="width"
         :collapsed-width="64"
-        :collapsed="collapsed"
+        :collapsed="leftCollapsed"
         :width="245"
         :native-scrollbar="false"
-        @collapse="collapsed = true"
-        @expand="collapsed = false" 
+        @collapse="leftCollapsed = true"
+        @expand="leftCollapsed = false"
       >
-        sider
+        sider (left)
       </n-layout-sider>
-      <n-layout-content>
-        tabs
-      </n-layout-content>
+
+      <n-layout has-sider sider-placement="right">
+        <n-layout-content>
+          tabs
+        </n-layout-content>
+
+        <n-layout-sider
+          bordered
+          show-trigger
+          collapse-mode="width"
+          :collapsed-width="64"
+          :collapsed="rightCollapsed"
+          :width="245"
+          :native-scrollbar="false"
+          @collapse="rightCollapsed = true"
+          @expand="rightCollapsed = false"
+        >
+          sider (right)
+        </n-layout-sider>
+      </n-layout>
     </n-layout>
+
     <n-layout-footer bordered style="flex-shrink: 0">
       footer
     </n-layout-footer>
   </n-layout>
 </template>
-  
+
 <script setup lang="ts">
+  import { ref } from 'vue';
+
+  const leftCollapsed = ref(false);
+  const rightCollapsed = ref(false);
 </script>
