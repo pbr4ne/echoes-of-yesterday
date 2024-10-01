5ec633ed9e49b01eb68ddc4300b7f721cb1a7906
pbr4ne <pbr4ne@protonmail.com>
2024-09-30 18:12:16 -0700

    feat: stats

--- src/components/GameSidebar.vue
+++ src/components/GameSidebar.vue
@@ -1,35 +1,81 @@
 <template>
-  <n-menu
-    :collapsed="collapsed"
-    :collapsed-width="64"
-    :collapsed-icon-size="24"
-    :icon-size="24"
-    :options="sidebar"
-  />
+  <n-space vertical size="medium" style="padding: 20px;">
+    <div v-for="(item, index) in sidebar" :key="item.key" class="sidebar-item">
+      <n-icon size="24" class="sidebar-icon">
+        <component :is="item.icon" />
+      </n-icon>
+      <div class="progress-wrapper">
+        <n-progress
+          v-if="!collapsed"
+          :percentage="item.percentage"
+          type="line"
+          :height="20"
+          color="#1e4378"
+          :show-indicator="false"
+        />
+        <div class="progress-label">
+          <span class="progress-text">{{ item.label }}</span>
+        </div>
+      </div>
+    </div>
+  </n-space>
 </template>
-    
+
 <script setup lang="ts">
-  import { computed } from 'vue';
-  import type { MenuOption } from 'naive-ui';
-  import { renderIcon } from '../composables/useIcon';
-  
-  import { FoodToast24Regular as HungerIcon } from '@vicons/fluent';
-  import { Coffee as ThirstIcon, DeviceTv as BoredomIcon } from '@vicons/tabler';
-  import { Sleep24Regular as FatigueIcon } from '@vicons/fluent';
-  import { ExclamationCircleOutlined as FearIcon } from '@vicons/antd';
+import { computed } from 'vue';
+import { NIcon } from 'naive-ui';
 
-  const { collapsed } = defineProps({
-    collapsed: {
-      type: Boolean,
-      required: true,
-    },
-  });
+// Import icons
+import { FoodToast24Regular as HungerIcon } from '@vicons/fluent';
+import { Coffee as ThirstIcon, DeviceTv as BoredomIcon } from '@vicons/tabler';
+import { Sleep24Regular as FatigueIcon } from '@vicons/fluent';
+import { ExclamationCircleOutlined as FearIcon } from '@vicons/antd';
 
-  const sidebar = computed<MenuOption[]>(() => [
-    { label: 'Hunger', key: 'hunger', icon: renderIcon(HungerIcon) },
-    { label: 'Thirst', key: 'thirst', icon: renderIcon(ThirstIcon) },
-    { label: 'Boredom', key: 'boredom', icon: renderIcon(BoredomIcon) },
-    { label: 'Fatigue', key: 'fatigue', icon: renderIcon(FatigueIcon) },
-    { label: 'Fear', key: 'fear', icon: renderIcon(FearIcon) }
-  ]);
+const { collapsed } = defineProps({
+  collapsed: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const sidebar = computed(() => [
+  { label: 'Hunger', key: 'hunger', icon: HungerIcon, percentage: 40 },
+  { label: 'Thirst', key: 'thirst', icon: ThirstIcon, percentage: 60 },
+  { label: 'Boredom', key: 'boredom', icon: BoredomIcon, percentage: 80 },
+  { label: 'Fatigue', key: 'fatigue', icon: FatigueIcon, percentage: 50 },
+  { label: 'Fear', key: 'fear', icon: FearIcon, percentage: 4 }
+]);
 </script>
+
+<style scoped>
+.sidebar-item {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.sidebar-icon {
+  margin-right: 8px;
+}
+
+.progress-wrapper {
+  position: relative;
+  flex-grow: 1;
+  margin-left: 8px;
+  width: 100%;
+}
+
+.progress-label {
+  position: absolute;
+  top: 0;
+  left: 10px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.n-progress {
+  width: 100%;
+}
+</style>
