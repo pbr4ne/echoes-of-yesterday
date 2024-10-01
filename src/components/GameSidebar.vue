c77513c975f4c9d19820fd5f948e89a4586eab5f
pbr4ne <pbr4ne@protonmail.com>
2024-10-01 14:06:22 -0700

    feat: research

--- src/components/GameSidebar.vue
+++ src/components/GameSidebar.vue
@@ -32,6 +32,21 @@
         {{ ghost.status }}
       </n-tag>
     </div>
+
+    <n-divider />
+
+    <div v-for="(r, index) in research" :key="r.key" class="sidebar-item">
+      <n-icon size="24" class="sidebar-icon">
+        <component :is="r.icon" />
+      </n-icon>
+      <span class="ghost-label" style="padding-left: 10px;" v-if="!collapsed">
+        {{ r.label }}
+      </span>
+      <n-tag :bordered="false" type="info">
+        {{ r.progress }}/10
+      </n-tag>
+    </div>
+
   </n-space>
 </template>
 
@@ -70,6 +85,14 @@ const ghosts = computed(() => [
   { label: 'Wraith', key: 'wraith', icon: WraithIcon, status: 'Encountered' },
   { label: 'Spirit', key: 'spirit', icon: SpiritIcon, status: 'Communicated' },
   { label: '???', key: 'apparition', icon: ApparitionIcon, status: 'Encountered' },
+]);
+
+const research = computed(() => [
+  { label: 'Cooking', key: 'cooking', icon: FearIcon, progress: 1 },
+  { label: 'Teamaking', key: 'teamaking', icon: FearIcon, progress: 3 },
+  { label: 'Conjuration', key: 'conjuration', icon: FearIcon, progress: 8 },
+  { label: 'Illusionary Visions', key: 'illusionaryVisions', icon: FearIcon, progress: 2 },
+  { label: 'Meditation', key: 'meditation', icon: FearIcon, progress: 10 },
 ]);
 </script>
 
