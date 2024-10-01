b63e3b7b7bd12450cb37fc70197e361e39829588
pbr4ne <pbr4ne@protonmail.com>
2024-09-30 18:47:37 -0700

    feat: ghosts

--- src/components/GameSidebar.vue
+++ src/components/GameSidebar.vue
@@ -10,7 +10,7 @@
           :percentage="item.percentage"
           type="line"
           :height="20"
-          color="#1e4378"
+          color="#43738B"
           :show-indicator="false"
         />
         <div class="progress-label">
@@ -18,18 +18,36 @@
         </div>
       </div>
     </div>
+    
+    <n-divider />
+
+    <div v-for="(ghost, index) in ghosts" :key="ghost.key" class="sidebar-item">
+      <n-icon size="24" class="sidebar-icon">
+        <component :is="ghost.icon" />
+      </n-icon>
+      <span class="ghost-label" style="padding-left: 10px;" v-if="!collapsed">
+        {{ ghost.label }}
+      </span>
+      <n-tag :type="ghost.status == 'Communicated'? 'warning' : 'info'" size="small" round v-if="!collapsed">
+        {{ ghost.status }}
+      </n-tag>
+    </div>
   </n-space>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue';
-import { NIcon } from 'naive-ui';
+import { NIcon, NTag } from 'naive-ui';
 
-// Import icons
 import { FoodToast24Regular as HungerIcon } from '@vicons/fluent';
 import { Coffee as ThirstIcon, DeviceTv as BoredomIcon } from '@vicons/tabler';
 import { Sleep24Regular as FatigueIcon } from '@vicons/fluent';
 import { ExclamationCircleOutlined as FearIcon } from '@vicons/antd';
+import { ChartBubble as OrbIcon } from '@vicons/tabler';
+import { WindySnow as WraithIcon } from '@vicons/carbon';
+import { Calibrate as PoltergeistIcon } from '@vicons/carbon';
+import { Atom2 as SpiritIcon } from '@vicons/tabler';
+import { AutoAwesomeOutlined as ApparitionIcon } from '@vicons/material';
 
 const { collapsed } = defineProps({
   collapsed: {
@@ -43,7 +61,15 @@ const sidebar = computed(() => [
   { label: 'Thirst', key: 'thirst', icon: ThirstIcon, percentage: 60 },
   { label: 'Boredom', key: 'boredom', icon: BoredomIcon, percentage: 80 },
   { label: 'Fatigue', key: 'fatigue', icon: FatigueIcon, percentage: 50 },
-  { label: 'Fear', key: 'fear', icon: FearIcon, percentage: 4 }
+  { label: 'Fear', key: 'fear', icon: FearIcon, percentage: 30 }
+]);
+
+const ghosts = computed(() => [
+  { label: 'Poltergeist', key: 'poltergeist', icon: PoltergeistIcon, status: 'Encountered' },
+  { label: 'Orb', key: 'orb', icon: OrbIcon, status: 'Encountered' },
+  { label: 'Wraith', key: 'wraith', icon: WraithIcon, status: 'Encountered' },
+  { label: 'Spirit', key: 'spirit', icon: SpiritIcon, status: 'Communicated' },
+  { label: '???', key: 'apparition', icon: ApparitionIcon, status: 'Encountered' },
 ]);
 </script>
 
@@ -52,6 +78,7 @@ const sidebar = computed(() => [
   display: flex;
   align-items: center;
   width: 100%;
+  margin-bottom: 8px;
 }
 
 .sidebar-icon {
@@ -75,7 +102,15 @@ const sidebar = computed(() => [
   align-items: center;
 }
 
+.ghost-label {
+  flex-grow: 1;
+}
+
 .n-progress {
   width: 100%;
+}
+
+.n-tag {
+  margin-left: auto;
 }
 </style>
