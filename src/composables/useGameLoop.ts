14e41d2c3b87129fec798a47dc29a9ce6a5b927f
pbr4ne <pbr4ne@protonmail.com>
2024-10-02 20:26:41 -0700

    feat: switch to requestAnimationFrame

--- src/composables/useGameLoop.ts
+++ src/composables/useGameLoop.ts
@@ -2,20 +2,31 @@ import { ref, onMounted, onBeforeUnmount } from 'vue';
 
 export function useGameLoop(ticksPerSecond = 20) {
   const tickCount = ref(0);
-  const intervalId = ref<number | null>(null);
+  let intervalId = ref<number | null>(null);
+  let lastTime = 0;
+
   const subscribers = ref(new Set<(tick: number) => void>());
 
   const startGameLoop = () => {
-    const interval = 1000 / ticksPerSecond;
-    intervalId.value = window.setInterval(() => {
-      tickCount.value += 1;
-      subscribers.value.forEach((callback) => callback(tickCount.value));
-    }, interval);
+    const tickInterval = 1000 / ticksPerSecond;
+    const loop = (time: number) => {
+      if (!lastTime) lastTime = time;
+      const delta = time - lastTime;
+
+      if (delta >= tickInterval) {
+        lastTime = time;
+        tickCount.value += 1;
+        subscribers.value.forEach((callback) => callback(tickCount.value));
+      }
+      intervalId.value = requestAnimationFrame(loop);
+    };
+
+    intervalId.value = requestAnimationFrame(loop);
   };
 
   const stopGameLoop = () => {
     if (intervalId.value !== null) {
-      clearInterval(intervalId.value);
+      cancelAnimationFrame(intervalId.value);
       intervalId.value = null;
     }
   };
