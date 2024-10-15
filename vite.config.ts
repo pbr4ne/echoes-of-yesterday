cfab1ed6da4ca568aa54df959b6fb72bde106dad
pbr4ne <pbr4ne@protonmail.com>
2024-10-15 16:47:11 -0700

    build: allow other hosts than localhost

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   base: '/echoes-of-yesterday/',
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
 });
