186e76d42503b430e01ba78d3b77d9d10a7dabbe
pbr4ne <pbr4ne@protonmail.com>
2024-10-15 16:47:38 -0700

    feat: rearrange header stuff, new icons

--- src/components/GameHeader.vue
+++ src/components/GameHeader.vue
@@ -1,10 +1,25 @@
 <template>
   <n-space justify="space-between">
-    <n-space style="padding-left: 5px;" width="100%" class="eoyHeader">
-      <span>{{ titleValue }}</span>
+    <n-space v-if="showTitle" style="padding-left: 5px;" width="100%" class="eoyHeader">
+      <span>Echoes of Yesterday</span>
     </n-space>
-    <n-space class="time-container" style="padding-top: 10px;">
-      <span class="timeHeader" style="padding-top: 35px;">
+    <n-space style="padding-top: 10px; padding-bottom: 8px; padding-left: 10px;">
+      <n-button dashed circle size="large">
+        <template #icon>
+          <component :is="renderIcon(RoomsIcon)" />
+        </template>
+      </n-button>
+      <n-button dashed circle size="large">
+        <template #icon>
+          <component :is="renderIcon(ProfileIcon)" />
+        </template>
+      </n-button>
+      <n-button dashed circle size="large">
+        <template #icon>
+          <component :is="renderIcon(ResearchIcon)" />
+        </template>
+      </n-button>
+      <span class="timeHeader" v-if="showTitle">
         Day {{ store.calendar.days }} 
         {{ String(store.calendar.hours).padStart(2, '0') }}:{{ String(Math.floor(store.calendar.minutes / 10) * 10).padStart(2, '0') }}
       </span>
@@ -16,18 +31,17 @@
 <script setup lang="ts">
   import { ref, onMounted, onBeforeUnmount } from 'vue';
   import { useStore } from '../composables/useStore'
+  import { Door as RoomsIcon } from '@vicons/tabler';
+  import { UserSearch as ProfileIcon } from '@vicons/tabler';
+  import { AccountTreeOutlined as ResearchIcon } from '@vicons/material';
   import { SunnyOutline as SunIcon } from '@vicons/ionicons5';
   import { renderIcon } from '../composables/useIcon';
 
-  const titleValue = ref(getTitleValue());
+  const showTitle = ref(window.innerWidth > 730);
   const store = useStore();
 
-  function getTitleValue() {
-    return window.innerWidth >= 450 ? 'Echoes of Yesterday' : 'EoY';
-  }
-
   const updateTitle = () => {
-    titleValue.value = getTitleValue();
+    showTitle.value = window.innerWidth > 730;
   };
 
   onMounted(() => {
@@ -40,8 +54,6 @@
 </script>
   
 <style scoped>
-
-
   .eoyHeader {
     font-family: 'Fondamento', sans-serif;
     font-size: 40px;
@@ -54,5 +66,7 @@
     font-size: 20px;
     padding-right: 5px;
     padding-left: 5px;
+    position: relative;
+    top: 3px;
   }
 </style>
