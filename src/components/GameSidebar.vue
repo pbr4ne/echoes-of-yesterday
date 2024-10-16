31b4f6b9edbbc4998ce385fddc471974f7d05292
pbr4ne <pbr4ne@protonmail.com>
2024-10-15 17:08:09 -0700

    feat: tweak dividers on sidebar

--- src/components/GameSidebar.vue
+++ src/components/GameSidebar.vue
@@ -1,14 +1,15 @@
 <template>
   <n-space vertical size="medium" style="padding: 20px;">
     <sidebar-stats :collapsed="collapsed" />
-    <n-divider />
+    <n-divider dashed :style="dividerStyle" />
     <sidebar-ghosts :collapsed="collapsed" />
-    <n-divider />
+    <n-divider dashed :style="dividerStyle" />
     <sidebar-research :collapsed="collapsed" />
   </n-space>
 </template>
 
 <script setup lang="ts">
+import { computed, ref, onMounted, onBeforeUnmount } from 'vue';
 import SidebarStats from './sidebar/SidebarStats.vue';
 import SidebarGhosts from './sidebar/SidebarGhosts.vue';
 import SidebarResearch from './sidebar/SidebarResearch.vue';
@@ -18,5 +19,23 @@ const { collapsed } = defineProps({
     type: Boolean,
     required: true,
   },
+});
+
+const screenHeight = ref(window.innerHeight);
+
+const dividerStyle = computed(() => {
+  return screenHeight.value < 780 ? { margin: '0' } : {};
+});
+
+const updateScreenHeight = () => {
+  screenHeight.value = window.innerHeight;
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateScreenHeight);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateScreenHeight);
 });
 </script>
