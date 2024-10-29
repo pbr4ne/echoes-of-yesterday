df8b3e755f751c7dfad3ec7f8fdb67eef6ca6cf8
pbr4ne <pbr4ne@protonmail.com>
2024-10-29 10:45:16 -0700

    feat: room tab tooltip

--- src/components/areas/Rooms.vue
+++ src/components/areas/Rooms.vue
@@ -14,12 +14,22 @@
       :disabled="room.locked"
     >
       <template #tab>
-        <n-icon :class="{ 'pulsate': isRoomActive(room.key) }" class="tab-icon">
-          <component :is="room.locked ? RoomLocked : room.icon" />
-        </n-icon>
-        <span v-if="showText" :class="{ 'pulsate': isRoomActive(room.key) }" class="tab-text">
-          {{ room.label }}
-        </span>
+        <n-tooltip trigger="hover">
+          <template #trigger>
+            <n-icon :class="{ 'pulsate': isRoomActive(room.key) }" class="tab-icon">
+              <component :is="room.locked ? RoomLocked : room.icon" />
+            </n-icon>
+          </template>
+          <span v-if="room.locked">Locked</span>
+        </n-tooltip>
+        <n-tooltip v-if="showText" trigger="hover">
+          <template #trigger>
+            <span :class="{ 'pulsate': isRoomActive(room.key) }" class="tab-text">
+              {{ room.label }}
+            </span>
+          </template>
+          <span v-if="room.locked">Locked</span>
+        </n-tooltip>
       </template>
 
       <component :is="room.component" />
