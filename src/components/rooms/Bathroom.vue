123eecfbbc2b942a29439b347e52928afbf31742
pbr4ne <pbr4ne@protonmail.com>
2025-12-10 12:47:34 -0800

    feat: bathroom areas

--- src/components/rooms/Bathroom.vue
+++ src/components/rooms/Bathroom.vue
@@ -7,10 +7,18 @@ import { ActionGroup } from '../../utilities/types';
 import Room from './Room.vue';
 
 const actionGroups: ActionGroup[] = [
-{
-    title: 'Test',
+  {
+    title: 'Bathtub',
     actions: [
-      { actionKey: 'stareAtFood', label: 'Test', duration: 10000, affected: [{ key: 'food', amount: 1 }] },
+      { actionKey: 'bathe', label: 'Bathe', duration: 10000, affected: [{ key: 'fatigue', amount: -10 }] },
+      { actionKey: 'readBook', label: 'Read Book', duration: 10000, affected: [{ key: 'boredom', amount: -10 }] },
+      { actionKey: 'sensoryDeprivation', label: 'Sensory Deprivation', duration: 10000, deviceKey: 'bathtub', affected: [] },
+    ],
+  },
+  {
+    title: 'Vanity',
+    actions: [
+      { actionKey: 'readSigils', label: 'Read Mirror Sigils', duration: 10000, deviceKey: 'mirror', affected: [] },
     ],
   },
 ];
