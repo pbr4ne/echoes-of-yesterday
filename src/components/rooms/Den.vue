dc71012c95297b4f8fa1dceefba8dcc914a8c06b
pbr4ne <pbr4ne@protonmail.com>
2025-12-10 13:25:29 -0800

    feat: den areas

--- src/components/rooms/Den.vue
+++ src/components/rooms/Den.vue
@@ -7,10 +7,20 @@ import { ActionGroup } from '../../utilities/types';
 import Room from './Room.vue';
 
 const actionGroups: ActionGroup[] = [
-{
-    title: 'Test',
+  {
+    title: 'Bookshelf',
     actions: [
-      { actionKey: 'stareAtFood', label: 'Test', duration: 10000, affected: [{ key: 'food', amount: 1 }] },
+      { actionKey: 'readNovel', label: 'Read Novel', duration: 10000, affected: [{ key: 'boredom', amount: -10 }] },
+      { actionKey: 'solvePuzzleBook', label: 'Solve Puzzle Book', duration: 10000, affected: [{ key: 'boredom', amount: -10 }] },
+      { actionKey: 'lookAtGhostBook', label: 'Look at Ghost Book', duration: 10000, deviceKey: 'ghostBook', affected: [] },
+    ],
+  },
+  {
+    title: 'Computer',
+    actions: [
+      { actionKey: 'readForums', label: 'Read Forums', duration: 10000, affected: [{ key: 'boredom', amount: -10 }] },
+      { actionKey: 'playSolitaire', label: 'Play Solitaire', duration: 10000, affected: [{ key: 'boredom', amount: -10 }] },
+      { actionKey: 'playComputerGame', label: 'Play Computer Game', duration: 10000, deviceKey: 'computerGame', affected: [] },
     ],
   },
 ];
