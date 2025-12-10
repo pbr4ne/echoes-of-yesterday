d9de9845ca995314d8348ef3cf09ff0cdbf8e0e5
pbr4ne <pbr4ne@protonmail.com>
2025-12-10 13:32:00 -0800

    feat: cellar areas

--- src/components/rooms/Cellar.vue
+++ src/components/rooms/Cellar.vue
@@ -7,10 +7,18 @@ import { ActionGroup } from '../../utilities/types';
 import Room from './Room.vue';
 
 const actionGroups: ActionGroup[] = [
-{
-    title: 'Test',
+  {
+    title: 'Corner',
     actions: [
-      { actionKey: 'stareAtFood', label: 'Test', duration: 10000, affected: [{ key: 'food', amount: 1 }] },
+      { actionKey: 'liftWeights', label: 'Lift Weights', duration: 10000, affected: [{ key: 'frailty', amount: -10 }] },
+      { actionKey: 'cleanEctoplasm', label: 'Clean Ectoplasm', duration: 10000, deviceKey: 'ectoplasm', affected: [] },
+    ],
+  },
+  {
+    title: 'Shelves',
+    actions: [
+      { actionKey: 'sortBoxes', label: 'Sort Boxes', duration: 10000, affected: [{ key: 'boredom', amount: -10 }, { key: 'frailty', amount: -10 }] },
+      { actionKey: 'useOuijaBoard', label: 'Use Ouija Board', duration: 10000, deviceKey: 'ouijaBoard', affected: [] },
     ],
   },
 ];
