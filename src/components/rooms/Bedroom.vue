90198d851deadcf6bc89d46f0948db2199a86446
pbr4ne <pbr4ne@protonmail.com>
2025-12-10 13:13:10 -0800

    feat: bedroom areas

--- src/components/rooms/Bedroom.vue
+++ src/components/rooms/Bedroom.vue
@@ -7,10 +7,21 @@ import { ActionGroup } from '../../utilities/types';
 import Room from './Room.vue';
 
 const actionGroups: ActionGroup[] = [
-{
-    title: 'Test',
+  {
+    title: 'Bed',
     actions: [
-      { actionKey: 'stareAtFood', label: 'Test', duration: 10000, affected: [{ key: 'food', amount: 1 }] },
+      { actionKey: 'nap', label: 'Nap', duration: 10000, affected: [{ key: 'fatigue', amount: -10 }] },
+      { actionKey: 'sleep', label: 'Sleep', duration: 10000, affected: [{ key: 'fatigue', amount: -10 }] },
+      { actionKey: 'read', label: 'Read', duration: 10000, affected: [{ key: 'boredom', amount: -10 }] },
+      { actionKey: 'watchStatic', label: 'Watch Static', duration: 10000, deviceKey: 'tv', affected: [] },
+    ],
+  },
+  {
+    title: 'Room',
+    actions: [
+      { actionKey: 'useBlackLight', label: 'Use Black Light', duration: 10000, deviceKey: 'blackLight', affected: [] },
+      { actionKey: 'lightCandles', label: 'Light Candles', duration: 10000, deviceKey: 'candles', affected: [] },
+      { actionKey: 'doCalisthenics', label: 'Do Calisthenics', duration: 10000, affected: [{ key: 'frailty', amount: -10 }] },
     ],
   },
 ];
